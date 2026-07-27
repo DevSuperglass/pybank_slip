f054e3bc951f670a0d4ef0c0769be70e4c32332c
Erick Fernando Martins Santos <erickmartinslima3@gmail.com>
2026-07-27 17:33:39 -0300

    fix: versão nova agora com um novo banco

--- src/pybank_slip/__init__.py
+++ src/pybank_slip/__init__.py
@@ -2,5 +2,5 @@ from .manager import BankSlipManager
 from .interfaces import BaseBankAdapter
 from .auth import CertificateAuth, OAuthCredentials
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["BankSlipManager", "BaseBankAdapter", "CertificateAuth", "OAuthCredentials"]
