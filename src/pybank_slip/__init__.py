730016417a9d8860763257cc499c54f610fe8161
Erick Fernando Martins Santos <erickmartinslima3@gmail.com>
2026-09-02 12:17:52 -0300

    nova versao

--- src/pybank_slip/__init__.py
+++ src/pybank_slip/__init__.py
@@ -2,5 +2,5 @@ from .manager import BankSlipManager
 from .interfaces import BaseBankAdapter
 from .auth import CertificateAuth, OAuthCredentials
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __all__ = ["BankSlipManager", "BaseBankAdapter", "CertificateAuth", "OAuthCredentials"]
